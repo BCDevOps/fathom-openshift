2b2e7cc800b427c14caa8bf804fa93cf00f7c87f
Emiliano Suñé <emiliano.sune@gmail.com>
2019-02-05 17:50:18 -0800

    Remove project-specific references
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,3 +1,3 @@
-export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-devex-von}
-export GIT_URI=${GIT_URI:-"https://github.com/esune/fathom-openshift.git"}
+export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-myproject}
+export GIT_URI=${GIT_URI:-"https://github.com/bcgov/fathom-openshift.git"}
 export GIT_REF=${GIT_REF:-"master"}
