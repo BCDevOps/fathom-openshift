383c16c494e7f81cf2e645054898c0954b088d0a
wadeking98 <wkingnumber2@gmail.com>
2021-01-06 13:33:31 -0800

    revert setting.sh back to default

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,3 +1,3 @@
-export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-4a9599}
-export GIT_URI=${GIT_URI:-"https://github.com/wadeking98/fathom-openshift.git"}
+export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-myproject}
+export GIT_URI=${GIT_URI:-"https://github.com/BCDevOps/fathom-openshift.git"}
 export GIT_REF=${GIT_REF:-"master"}
